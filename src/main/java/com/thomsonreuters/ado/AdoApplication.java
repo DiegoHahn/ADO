5880b8726f02a00e08d1d16da88f65108aa4f193
Diego Hahn <diego-hf@hotmail.com>
2024-08-19 14:45:32 -0300

    Testando java ado

--- src/main/java/com/thomsonreuters/ado/AdoApplication.java
+++ src/main/java/com/thomsonreuters/ado/AdoApplication.java
@@ -1,13 +1,23 @@
 package com.thomsonreuters.ado;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.microsoft.azure.devops.AzureDevOpsClient;
+import com.microsoft.azure.devops.models.WorkItem;
+import java.util.List;
 
-@SpringBootApplication
-public class AdoApplication {
+public class AzureDevOpsExample {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdoApplication.class, args);
-	}
+		String organizationUrl = "https://dev.azure.com/your-organization";
+		String personalAccessToken = "your-personal-access-token";
 
+		AzureDevOpsClient client = new AzureDevOpsClient(organizationUrl, personalAccessToken);
+
+		// Executa a consulta para trazer os Work Items atribuídos a você
+		String wiqlQuery = "SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.AssignedTo] = @Me";
+		List<WorkItem> workItems = client.getWorkItems(wiqlQuery);
+
+		for (WorkItem workItem : workItems) {
+			System.out.println("ID: " + workItem.getId() + ", Title: " + workItem.getFields().get("System.Title"));
+		}
+	}
 }
