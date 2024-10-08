b7cda9bb329e203570978a0a09e0b59af2c135c5
Diego Hahn <diego-hf@hotmail.com>
2024-10-08 17:57:35 -0300

    classe de req

---
+++ src/main/java/com/thomsonreuters/ado/Model/EmailRequest.java
@@ -0,0 +1,19 @@
+package com.thomsonreuters.ado.Model;
+
+public class EmailRequest {
+    private String email;
+
+    public EmailRequest() {}
+
+    public EmailRequest(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
